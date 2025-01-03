d6de423794bffc3f3e9169606eb74b6dcc6d66d3
akshay agnihotri <agnihotriakshay36@gmail.com>
2025-01-03 20:53:31 +0530

    created first project

--- 05_project_01/server.js
+++ 05_project_01/server.js
@@ -2,12 +2,9 @@ const express = require("express");
 const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 8000;
+const fs = require("fs");
 
 // Routes
-app.get("/api/users", (req, res) => {
-  return res.json(users);
-});
-
 app.get("/users", (req, res) => {
   const html = `
       <ul>
@@ -16,6 +13,22 @@ app.get("/users", (req, res) => {
     `;
   return res.send(html);
 });
+
+app.use(express.urlencoded({ extended: false }));
+
+app
+  .route("/api/users")
+  .get((req, res) => {
+    return res.json(users);
+  })
+  .post((req, res) => {
+    //TODO: Create new user
+    const body = req.body;
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+      return res.json({ status: "sucess", id: users.length });
+    });
+  });
 
 app
   .route("/api/users/:id")
@@ -26,6 +39,26 @@ app
   })
   .put((req, res) => {
     return res.end("hello this is a post request");
+  })
+  .patch((req, res) => {
+    const { id } = req.params;
+    const updates = req.body;
+    const userIndex = users.findIndex((user) => user.id === parseInt(id));
+    if (userIndex === -1) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    users[userIndex] = { ...users[userIndex], ...updates };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) throw err;
+      return res.send({ status: "done", data: users[userIndex] });
+    });
+  })
+  .delete((req, res) => {
+    const updatedUsers = users.filter((user) => user.id !== +req.params.id);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(updatedUsers), (err) => {
+      if (err) throw err;
+      return res.send({ status: "success" });
+    });
   });
 
 app.listen(PORT, () => {
